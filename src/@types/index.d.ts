ecdccaa9c6d72f2c04ee1d8aa65194655addf6aa
mato533 <35281743+mato533@users.noreply.github.com>
2023-06-02 00:38:24 +0900

    feat: Add the main logic of this project
    Added process to generate bundle file to deploy to GoogleAppsScript for
    rollup.

---
+++ src/@types/index.d.ts
@@ -0,0 +1,8 @@
+declare module "gas-entry-generator";
+
+interface PluginOption {
+  comment?: boolean;
+  autoGlobalExports?: boolean;
+  exportsIdentifierName?: string;
+  globalIdentifierName?: string;
+}
