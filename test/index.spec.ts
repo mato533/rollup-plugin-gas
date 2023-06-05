17470e721802aa8af7e9289b17aa862055e6834e
mato533 <35281743+mato533@users.noreply.github.com>
2023-06-05 12:02:45 +0900

    refactor: Commonize test logic for generating Fixture paths
    In order to respond to the increasing number of test cases, we will
    commonize the logic that generate the path of the FixtureFile in the
    test.

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -6,12 +6,16 @@ import { describe, it } from "vitest";
 
 const dirFixtures = path.resolve(__dirname, "./fixtures");
 
+const defineFixtureFileName = (scenario: string) => {
+  const inputFile = path.join(dirFixtures, `${scenario}.js`);
+  const outputFile = path.join(dirFixtures, `${scenario}.bundle.js`);
+  return { inputFile, outputFile };
+};
 describe("rollup-plugin-gas", () => {
   it.each(["basic", "allow-function"])(
     "should entry point function is added. #%s",
     async (scenario) => {
-      const inputFile = path.join(dirFixtures, `${scenario}.js`);
-      const outputFile = path.join(dirFixtures, `${scenario}.bundle.js`);
+      const { inputFile, outputFile } = defineFixtureFileName(scenario);
       const bundle = await rollup({
         input: inputFile,
         plugins: [rollupPluginGas()],
