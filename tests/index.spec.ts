71cd37e8fa0a161e9fc30a283f0935bbf48e3e74
mato533 <35281743+mato533@users.noreply.github.com>
2023-06-17 10:02:02 +0900

    test: Add the test for logic to copy the manifest file

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,9 +1,11 @@
 import path from "path";
 import fs from "fs";
 import { rollup } from "rollup";
+import type { OutputAsset } from "rollup";
 import { describe, expect, it } from "vitest";
 import rollupPluginGas from "../src";
-import type { RollupPluginGasOption } from "types";
+import type { RollupPluginGasOptions } from "types";
+import { manifest } from "@/plugin-manifest";
 
 interface TestParams {
   scenario: string;
@@ -11,6 +13,7 @@ interface TestParams {
 }
 
 const dirFixtures = path.resolve(__dirname, "./fixtures");
+const dirIncludeFixtures = path.resolve(__dirname, "./fixtures/include");
 
 const defineFixtureFileName = (param: TestParams) => {
   const { scenario, dirFixtures: dir } = param;
@@ -19,23 +22,47 @@ const defineFixtureFileName = (param: TestParams) => {
   return { inputFile, outputFile };
 };
 
-const buildAndAssertOutput = async (
-  param: TestParams,
-  options?: RollupPluginGasOption
-) => {
-  const { inputFile, outputFile } = defineFixtureFileName(param);
-
+const build = async (inputFile: string, options?: RollupPluginGasOptions) => {
   const bundle = await rollup({
     input: inputFile,
     plugins: [rollupPluginGas(options)],
   });
 
-  const output = await bundle.generate({});
+  return await bundle.generate({});
+};
+
+const buildAndAssertOutput = async (
+  param: TestParams,
+  options?: RollupPluginGasOptions
+) => {
+  const { inputFile, outputFile } = defineFixtureFileName(param);
+
+  const output = await build(inputFile, options);
+
+  expect(output.output.length).toBe(1);
   const [{ code: generated }] = output.output;
   const expected = fs.readFileSync(outputFile, {
     encoding: "utf8",
   });
   expect(generated).toBe(expected);
+};
+
+const buildAndAssertManifest = async (option: RollupPluginGasOptions) => {
+  const { inputFile } = defineFixtureFileName({
+    scenario: "basic",
+    dirFixtures: dirFixtures,
+  });
+
+  const expected = fs.readFileSync(path.join(dirFixtures, manifest), {
+    encoding: "utf8",
+  });
+
+  const output = await build(inputFile, option);
+  expect(output.output.length).toBe(2);
+  const generatedManifest = output.output[1] as OutputAsset;
+  expect(generatedManifest.type).toBe("asset");
+  expect(generatedManifest.fileName).toBe(manifest);
+  expect(generatedManifest.source).toBe(expected);
 };
 
 describe("rollup-plugin-gas", () => {
@@ -49,7 +76,7 @@ describe("rollup-plugin-gas", () => {
     }
   );
 
-  it("Should includ comments", async () => {
+  it("Should include comments", async () => {
     await buildAndAssertOutput(
       { scenario: "comment", dirFixtures: dirFixtures },
       { comment: true }
@@ -57,7 +84,6 @@ describe("rollup-plugin-gas", () => {
   });
 
   it("Should add entrypoint function of the specified file", async () => {
-    const dirIncludeFixtures = path.resolve(__dirname, "./fixtures/include");
     await buildAndAssertOutput(
       { scenario: "include", dirFixtures: dirIncludeFixtures },
       { include: ["**/include.js"] }
@@ -72,5 +98,31 @@ describe("rollup-plugin-gas", () => {
       },
       { moduleHeaderComment: true }
     );
+  });
+
+  it("Should print each source filename to bandle file", async () => {
+    await buildAndAssertOutput(
+      {
+        scenario: "include-comment-module",
+        dirFixtures: dirIncludeFixtures,
+      },
+      { include: ["**/include-comment-module.js"], moduleHeaderComment: true }
+    );
+  });
+
+  it("Should copy the manifest file", async () => {
+    vi.spyOn(process, "cwd").mockReturnValue(dirFixtures);
+
+    await buildAndAssertManifest({
+      manifest: { copy: true },
+    });
+  });
+
+  it("Should cgit opy the manifest file using srcDir option", async () => {
+    vi.spyOn(process, "cwd").mockReturnValue(__dirname);
+
+    await buildAndAssertManifest({
+      manifest: { copy: true, srcDir: "./fixtures" },
+    });
   });
 });
