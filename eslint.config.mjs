d7dfb509730049a467f104f3772287e5c5e9f10b
mato533 <35281743+mato533@users.noreply.github.com>
2024-10-25 17:37:44 +0900

    chore: update eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import importPlugin from "eslint-plugin-import";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
@@ -7,6 +9,46 @@ export default tseslint.config(
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
 
+  {
+    plugins: {
+      unicorn: eslintPluginUnicorn,
+      import: importPlugin,
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+          disallowTypeAnnotations: false,
+        },
+      ],
+      "unicorn/prefer-node-protocol": "error",
+      "import/no-duplicates": "error",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            ["parent", "sibling"],
+            "index",
+            "object",
+            "type",
+          ],
+          distinctGroup: true,
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "parent",
+              position: "before",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
   {
     ignores: [
       "tests/__fixtures__/**",
